2799f63079db911a2cc4bdaeca804b83991b78d8
motekito <kovernik@sofbalance.ru>
2017-09-21 11:44:15 +0300

    Добавлен тест по изменению пользвателей

--- fixture/user.py
+++ fixture/user.py
@@ -4,7 +4,7 @@ class UserHelper:
 
     def create(self, user):
         wd = self.app.wd
-        # init use r creation
+        # init user creation
         wd.find_element_by_link_text("add new").click()
         # fill user form
         wd.find_element_by_name("firstname").click()
@@ -37,3 +37,40 @@ class UserHelper:
         wd.find_element_by_name("email").send_keys(user.email)
         # submit user creation
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def modify(self, user):
+        wd = self.app.wd
+        # init user modify
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[36]/td[8]/a/img").click()
+        # fill user form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(user.name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(user.surname)
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").send_keys(user.nickname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(user.middle)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(user.address)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(user.company)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(user.phone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(user.mobile)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(user.email)
+        # submit user modify
+        wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
+
