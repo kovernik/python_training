e69f9861ab631802004dc95b63f08b5ca9385e22
motekito <kovernik@sofbalance.ru>
2017-09-21 10:33:57 +0300

    Создана общая фикстура

--- conftest.py
+++ conftest.py
@@ -1,17 +1,9 @@
 import pytest
 from fixture.application import Application
-from fixture.user_application import User_application
 
 
 @pytest.fixture(scope="session")
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-
-@pytest.fixture(scope="session")
-def app_user(request):
-    fixture = User_application()
     request.addfinalizer(fixture.destroy)
     return fixture
--- test/test_add_user.py
+++ test/test_add_user.py
@@ -1,16 +1,16 @@
 from user import User
 
 
-def test_add_user(app_user):
-    app_user.session.login(username="admin", password="secret")
-    app_user.user.create(User(name="Name", surname="Last name", email="kovernik@softbalance.ru", mobile="+79110000000",
+def test_add_user(app):
+    app.session.login(username="admin", password="secret")
+    app.user.create(User(name="Name", surname="Last name", email="kovernik@softbalance.ru", mobile="+79110000000",
                          phone="+78120001110", company="SoftBalance", address="Shaumyana, 55", middle="Middle",
                          nickname="nickname"))
-    app_user.session.logout()
+    app.session.logout()
 
 
-def test_add_empty_user(app_user):
-    app_user.session.login(username="admin", password="secret")
-    app_user.user.create(User(name="", surname="", email="", mobile="", phone="", company="", address="", middle="",
+def test_add_empty_user(app):
+    app.session.login(username="admin", password="secret")
+    app.user.create(User(name="", surname="", email="", mobile="", phone="", company="", address="", middle="",
                          nickname=""))
-    app_user.session.logout()
+    app.session.logout()
