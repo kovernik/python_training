f4a0449c919fbac7a2cc8efdbc6ca6a9cc4abc11
motekito <kovernik@sofbalance.ru>
2017-09-21 11:18:12 +0300

    Добавлен тест на модифицирование групп

--- fixture/group.py
+++ fixture/group.py
@@ -25,6 +25,26 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def modify(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # init group modify
+        wd.find_element_by_xpath("//div[@id='content']/form/span[12]/input").click()
+        wd.find_element_by_name("edit").click()
+        # modify group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # submit group modify
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
