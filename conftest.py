f622be97efebb2dc6359c163ccc445d718453d33
motekito <kovernik@sofbalance.ru>
2017-09-25 09:54:52 +0300

    Рлизована проверка валидности фикстуры и проверка состояния сессии

--- conftest.py
+++ conftest.py
@@ -1,9 +1,28 @@
 import pytest
 from fixture.application import Application
 
+fixture = None
 
-@pytest.fixture(scope="session")
+
+@pytest.fixture
 def app(request):
+<<<<<<< HEAD
+    global fixture
+    if fixture is None:
+        fixture = Application()
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+    fixture.session.ensure_login(username="admin", password="secret")
+    return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
+    def fin():
+        fixture.session.ensure_logout()
+        fixture.destroy()
+=======
     fixture = Application()
     fixture.session.login(username="admin", password="secret")
 
@@ -11,5 +30,6 @@ def app(request):
         fixture.session.logout()
         fixture.destroy()
 
+>>>>>>> 379862cfbe3f982f0bcce2346d6dd7c93debd0f7
     request.addfinalizer(fin)
     return fixture
