e378e853cca4498c0dab54f1d8f5bfa02734fdaf
motekito <kovernik@sofbalance.ru>
2017-09-25 10:19:54 +0300

    Откатился на рабочем ПК

--- conftest.py
+++ conftest.py
@@ -1,28 +1,9 @@
 import pytest
 from fixture.application import Application
 
-fixture = None
 
-
-@pytest.fixture
+@pytest.fixture(scope="session")
 def app(request):
-<<<<<<< HEAD
-    global fixture
-    if fixture is None:
-        fixture = Application()
-    else:
-        if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
-    return fixture
-
-
-@pytest.fixture(scope="session", autouse=True)
-def stop(request):
-    def fin():
-        fixture.session.ensure_logout()
-        fixture.destroy()
-=======
     fixture = Application()
     fixture.session.login(username="admin", password="secret")
 
@@ -30,6 +11,5 @@ def stop(request):
         fixture.session.logout()
         fixture.destroy()
 
->>>>>>> 379862cfbe3f982f0bcce2346d6dd7c93debd0f7
     request.addfinalizer(fin)
     return fixture
