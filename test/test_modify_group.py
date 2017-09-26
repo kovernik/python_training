12a9f2f306013bb0c768b00d41f118485fea5410
motekito <kovernik@sofbalance.ru>
2017-09-26 10:38:52 +0300

    Добавлена проверка перед модификаций групп и контактов

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,8 +4,4 @@ from model.group import Group
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="new"))
-    app.group.modify_first_group()
-
-
-# def test_modify_group_header(app):
-#    app.group.modify_first_group(Group(header="New header"))
+    app.group.modify_first_group(Group(name="New group"))
--- test/test_modify_user.py
+++ test/test_modify_user.py
@@ -6,4 +6,6 @@ def test_modify_user(app):
         app.user.create(
             User(name="new", surname="new", email="new", mobile="new", phone="new", company="new", address="new",
                  middle="new", nickname="new"))
-    app.user.modify()
+    app.user.modify(
+        User(name="new1", surname="new1", email="new1", mobile="new1", phone="new1", company="new1", address="new1",
+             middle="new1", nickname="new1"))
