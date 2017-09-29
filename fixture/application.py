07577bdaa72a81a6e7f55dae3327847388e15414
motekito <kovernik@sofbalance.ru>
2017-09-29 16:56:26 +0300

    Оптимизированы переходы для контактов

--- fixture/application.py
+++ fixture/application.py
@@ -20,11 +20,15 @@ class Application:
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
+        if not (wd.current_url == "http://localhost/addressbook/" and len(
+                wd.find_elements_by_xpath("//input[@value='Delete']"))):
+            wd.get("http://localhost/addressbook/")
 
     def return_to_home_page(self):
         wd = self.wd
-        wd.find_element_by_link_text("home page").click()
+        if not (wd.current_url == "http://localhost/addressbook/" and len(
+                wd.find_elements_by_xpath("//input[@value='Delete']"))):
+            wd.find_element_by_link_text("home page").click()
 
     def destroy(self):
         self.wd.quit()
