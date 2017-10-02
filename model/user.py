b751f9c7bb20a016b830d2dbb123dccd831f2c8e
motekito <motekito@gmail.com>
2017-10-02 21:49:06 +0300

    Непонятая ошибка

--- model/user.py
+++ model/user.py
@@ -11,3 +11,9 @@ class User:
         self.middle = middle
         self.nickname = nickname
         self.id = id
+
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
--- test/test_del_user.py
+++ test/test_del_user.py
@@ -6,4 +6,9 @@ def test_delete_user(app):
         app.user.create(
             User(name="new", surname="new", email="new", mobile="new", phone="new", company="new", address="new",
                  middle="new", nickname="new"))
-    app.user.delete()
+    old_users = app.user.get_user_list()
+    app.user.test_delete_user()
+    new_users = app.user.get_user_list()
+    assert len(old_users) - 1 == len(new_users)
+    old_users[0:1] = []
+    assert old_users == new_users
